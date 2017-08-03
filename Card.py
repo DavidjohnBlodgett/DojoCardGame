9fad233d3b9b5f78058c8ffc73e68ae3df9272b3
Branaman <Mbranaman89@gmail.com>
2017-08-03 13:55:56 -0700

    removed description and type from card. Not needed for MVP

--- Card.py
+++ Card.py
@@ -1,17 +1,13 @@
 class Card(object):
-    def __init__(self,name,cast,disc,type,atk,defc):
+    def __init__(self,name,cast,atk = false,defc = false):
         self.name = name
         self.cast = cast
-        self.disc = disc
-        self.type = type
         self.atk = atk
         self.defc = defc
     def debug(self):
         print "#### {0} Debug Output ####".format(self.name)
         print "# Name:",self.name
         print "# Cast:",self.cast
-        print "# Description:",self.disc
-        print "# Type:",self.type
         print "# Attack:",self.atk
         print "# Defence",self.defc
         print "##########################"
